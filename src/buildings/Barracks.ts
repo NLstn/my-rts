09399caec8a8bea2ff2e880b9b34e3494e63019c
Niklas <niklas.lahnstein@gmail.com>
2025-11-21 14:38:30 +0000

    Implement building placement mode with validation and create Base and Barracks classes

---
+++ src/buildings/Barracks.ts
@@ -0,0 +1,16 @@
+import Phaser from 'phaser';
+import { Building, type BuildingConfig } from './Building';
+
+export class Barracks extends Building {
+    constructor(scene: Phaser.Scene) {
+        const config: BuildingConfig = {
+            name: 'Barracks',
+            width: 100,
+            height: 80,
+            color: 0x3366ff,
+            cost: 50,
+            type: 'military',
+        };
+        super(scene, config);
+    }
+}
