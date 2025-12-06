beddc5e47b3546f87b84a6245b4e57633eae15ad
Niklas Lahnstein <niklas.lahnstein@gmail.com>
2025-12-06 08:42:07 +0000

    fix: Update ESLint config for v9 compatibility
    - Add ignores for dist and node_modules directories
    - Update lint script to use modern ESLint CLI (remove --ext flag)
    - ESLint 9 requires flat config format

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import tsPlugin from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 
 export default [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
   js.configs.recommended,
   {
     files: ['src/**/*.ts'],
